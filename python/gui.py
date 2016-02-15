d9ffecd56b5a2cdd4c0be28af1a014bdf388081a
WenchenLi <largemaths@gmail.com>
2016-02-15 10:27:18 -0500

    finish load image and display, need to use embed opencv window in tkinter instead of Tkinter.Label

--- python/gui.py
+++ python/gui.py
@@ -1,45 +1,31 @@
 import Tkinter as tk
-import ImageTk
-import Image
-import cv2
 import os
-path ='pyhton/rest-samp-before-2-208x300.jpg'
-root = tk.Tk()
+import numpy as np
+import cv2
+import Tkinter
+import Image, ImageTk
+from Tkinter import Tk
+from tkFileDialog import askopenfilename
+from inpainting import *
 
 def filePicker():
-    from Tkinter import Tk
-    from tkFileDialog import askopenfilename
-    Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
-    filename = askopenfilename() # show an "Open" dialog box and return the path to the selected file
-    print filename
+    Tk().withdraw()
+    filename = askopenfilename()
     return filename
-import_button = tk.Button(root,text="select photo",command=(lambda : filePicker())).pack(fill=tk.X, side = tk.TOP, )
+def SelectAndShowImage(root):
+    img = cv2.imread(filePicker())
+    b,g,r = cv2.split(img)#Rearrang the color channel
+    img = cv2.merge((r,g,b))
+    im = Image.fromarray(img)# Convert the Image object into a TkPhoto object
+    imgtk = ImageTk.PhotoImage(image=im)
+    Tkinter.Label(root, image=imgtk).pack()#http://stackoverflow.com/questions/21979172/tkinter-display-other-windows
+    root.mainloop()
+
+global p_image,p_mask
+root = tk.Tk()
+
+import_button = tk.Button(root, text="select photo", command=(lambda : SelectAndShowImage(root))).pack(fill=tk.X, side = tk.TOP, )
 pick_button = tk.Button(root,text="pick area").pack(fill=tk.X, side = tk.TOP)
-restore_buttion = tk.Button(root,text="restore").pack(fill=tk.X, side = tk.TOP)
+restore_button = tk.Button(root,text="restore",command=(lambda : inpaint(p_image,p_mask))).pack(fill=tk.X, side = tk.TOP)
 
-embed = tk.Frame(root, width=640, height=400)
-embed.pack()
-# Tell pygame's SDL window which window ID to use
-os.environ['SDL_WINDOWID'] = str(embed.winfo_id())
-# The wxPython wiki says you might need the following line on Windows
-# (http://wiki.wxpython.org/IntegratingPyGame).
-#os.environ['SDL_VIDEODRIVER'] = 'windib'
-root.update()
-
-# cropimage = cropImage(path)
-# # output_loc = 'out.png'
-# cropimage.setup(path)
-# left, upper, right, lower = cropimage.mainLoop()
-# # ensure output rect always has positive width, height
-# if right < left:
-#     left, right = right, left
-# if lower < upper:
-#     lower, upper = upper, lower
-# im = Image.open(path)
-# im = im.crop(( left, upper, right, lower))
-# pygame.display.quit()
-# im.save(cropimage)
-# img = ImageTk.PhotoImage(Image.open(path))
-# panel = tk.Label(root, image = img)
-# panel.pack(side = "bottom", fill = "both", expand = "yes")
 root.mainloop()
--- python/inpainting.py
+++ python/inpainting.py
@@ -3,18 +3,19 @@ import numpy as np
 import cv2
 import sys
 
-
-def main(argv):
-	if len(sys.argv) < 3:
-		print "Not enough params"
-		sys.exit()
-	img = cv2.imread(sys.argv[1])
-	mask = cv2.imread(sys.argv[2],0)
-	res = cv2.inpaint(img,mask,3,cv2.INPAINT_NS) 
-	cv2.imshow('res',res)
-	cv2.waitKey(10000)
-	cv2.destroyAllWindows()
-	sys.exit()
+def inpaint(p_image, p_mask):
+    if len(sys.argv) < 3:
+        print "Not enough params"
+        sys.exit()
+    img = cv2.imread(p_image)
+    mask = cv2.imread(p_mask, 0)
+    res = cv2.inpaint(img, mask, 3, cv2.INPAINT_NS)
+    cv2.imshow('res', res)
+    cv2.waitKey(10000)
+    cv2.destroyAllWindows()
+    sys.exit()
 
 if __name__ == "__main__":
-   main(sys.argv[1:])
+    p_image = ""
+    p_mask =""
+    inpaint(p_image, p_mask)
