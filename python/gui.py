bda3026b8249c5bc3f51781dd242851d68dd4604
Avery Allen <averyallen71>
2016-02-21 16:31:38 -0500

    adds toolbox.  Still WIP.

--- python/gui.py
+++ python/gui.py
@@ -10,8 +10,57 @@ from inpainting import *
 
 def filePicker():
     tk.Tk().withdraw()
-    filename = askopenfilename()
+    filename = askopenfilename(initialdir=os.path.join( os.getcwd(), '../data/images' ))
     return filename
+
+
+class ToolBar():
+	def __init__(self,master):
+		self.master = master
+		self.frame = tk.Frame(self.master)
+		self.canvas = tk.Canvas(master, width=130, height=300)
+		self.canvas.pack()
+		self.master.title("Toolbar")
+		self.master.maxsize(130,300)
+		self.master.minsize(130,300)
+
+		self.brush_selected_image = tk.PhotoImage(file="../assets/paint_selected.png")
+		self.brush_not_selected_image = tk.PhotoImage(file="../assets/paint_not_selected.png")
+		self.rectangle_selected_image = tk.PhotoImage(file="../assets/rectangle_selected.png")
+		self.rectangle_not_selected_image = tk.PhotoImage(file="../assets/rectangle_not_selected.png")
+		
+		self.brush_button = tk.Button(self.canvas, image=self.brush_selected_image,command=(self.select_paintbrush))
+		self.brush_button.pack(side=tk.LEFT,padx=5,pady=5)
+		self.rectangle_button = tk.Button(self.canvas, image=self.rectangle_not_selected_image,command=(self.select_rectangle_tool))
+		self.rectangle_button.pack(side=tk.RIGHT,padx=5,pady=5)
+		
+		self.rectangle_selected = False
+		self.brush_selected = True
+
+
+	def select_paintbrush(self):
+		if not self.brush_selected:
+			self.brush_button["image"] = self.brush_selected_image
+			self.brush_selected = True
+
+			self.rectangle_button["image"] = self.rectangle_not_selected_image
+			self.rectangle_selected = False
+
+
+	def select_rectangle_tool(self):
+		if not self.rectangle_selected:
+			self.rectangle_button["image"] = self.rectangle_selected_image
+			self.rectangle_selected = True
+			
+			self.brush_button["image"] = self.brush_not_selected_image
+			self.brush_selected = False
+	
+	def select_paintbrush_size(self):
+		pass
+
+	def close_window(self):
+		self.master.destroy()
+
 
 class App():
     def __init__(self, master):
@@ -26,7 +75,7 @@ class App():
         self.canvas = tk.Canvas(master, width=w, height=h)
         self.canvas.pack()
         self.x = 0
-        self.y =0
+        self.y = 0
         self.select_windows=[]
         self.current_window=[]
 
@@ -43,6 +92,9 @@ class App():
         # pick_button = tk.Button(self.master,text="select masks").pack(fill=tk.X, side = tk.TOP)
         save_button = tk.Button(self.master,text="save masks",command=(self.save_mask)).pack(fill=tk.X, side = tk.TOP)#this is intermediate , will delete later
         restore_button = tk.Button(self.master,text="restore",command=(lambda : inpaint(self.file,self.mask_path))).pack(fill=tk.X, side = tk.TOP)
+        
+        toolbar = ToolBar(tk.Toplevel(self.master))
+
         self.master.mainloop()
 
     def crop_face(self):
@@ -68,7 +120,7 @@ class App():
             scaleFactor=1.1,
             minNeighbors=5,
             minSize=(30, 30),
-            flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+            flags = cv2.CASCADE_SCALE_IMAGE
         )
 
         print "Found {0} faces!".format(len(faces))
