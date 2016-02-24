c4363f292f0b355ec5959534940820660a8729cd
WenchenLi <largemaths@gmail.com>
2016-02-23 19:02:39 -0500

    integrate toolbar together with canvas for easy handling instance values. finish brush mask directly on image, need to make scroller work for large image.

--- python/gui.py
+++ python/gui.py
@@ -1,6 +1,8 @@
 import Tkinter as tk
 from PIL import Image, ImageTk, ImageOps, ImageDraw
 import os
+from Tkconstants import LEFT, RIGHT, Y, END, BOTH, VERTICAL
+
 import numpy as np
 import cv2
 
@@ -11,7 +13,7 @@ from inpainting import *
 
 def filePicker():
     tk.Tk().withdraw()
-    filename = askopenfilename(initialdir=os.path.join(os.getcwd(), '../data/images'))
+    filename = askopenfilename(initialdir=os.path.join(os.getcwd(), '../data/images/original/'))
     return filename
 
 class ToolBar():
@@ -38,7 +40,7 @@ class ToolBar():
         self.brush_size = 5
 
         self.brush_slider = tk.Scale(self.master, from_=1, to=10, orient=tk.HORIZONTAL, variable=self.brush_size,
-                                     command=(self.change_slider_value))
+                                     command=(lambda: self.change_slider_value))
         self.brush_slider.set(5)
         self.brush_slider.pack()
 
@@ -96,7 +98,49 @@ class ToolBar():
 class App():
     def __init__(self, master):
         self.master = master
-        self.toolbar = ToolBar(tk.Toplevel(self.master))
+
+        scrollbar = tk.Scrollbar(self.master,orient=VERTICAL)
+        scrollbar.pack(side=RIGHT, fill=Y)
+
+        # self.toolbar = ToolBar(tk.Toplevel(self.master))
+        self.frame = tk.Frame(self.master)
+        self.frame.pack()
+        self.canvas = tk.Canvas(master, width=130, height=300)
+        self.canvas.pack()
+        save_button = tk.Button(self.master, text="save masks", command=(self.save_mask)).pack(fill=tk.X,
+                                                                                               side=tk.TOP)  # this is intermediate , will delete later
+        restore_button = tk.Button(self.master, text="restore",
+                                   command=(lambda: inpaint(self.file, self.mask_path))).pack(fill=tk.X, side=tk.TOP)
+
+        # self.master.title("Toolbar")
+        # self.master.maxsize(130, 300)
+        # self.master.minsize(130, 300)
+
+        self.brush_selected_image = tk.PhotoImage(file="../assets/paint_selected.png")
+        self.brush_not_selected_image = tk.PhotoImage(file="../assets/paint_not_selected.png")
+        self.rectangle_selected_image = tk.PhotoImage(file="../assets/rectangle_selected.png")
+        self.rectangle_not_selected_image = tk.PhotoImage(file="../assets/rectangle_not_selected.png")
+
+        self.brush_button = tk.Button(self.canvas, image=self.brush_selected_image, command=(self.select_paintbrush))
+        self.brush_button.pack(side=tk.LEFT, padx=5, pady=5)
+        self.rectangle_button = tk.Button(self.canvas, image=self.rectangle_not_selected_image,
+                                          command=(self.select_rectangle_tool))
+        self.rectangle_button.pack(side=tk.RIGHT, padx=5, pady=5)
+
+        self.brush_size = 5
+
+        self.brush_slider = tk.Scale(self.master, from_=1, to=10, orient=tk.HORIZONTAL, variable=self.brush_size,
+                                     command=(self.change_slider_value))
+        self.brush_slider.set(5)
+        self.brush_slider.pack()
+
+        self.brush_size_image = tk.PhotoImage(file="../assets/brushsize_4.png")
+        self.brush_size_label = tk.Label(self.master, image=self.brush_size_image)
+        self.brush_size_label.pack()
+
+        self.rectangle_selected = False
+        self.brush_selected = True
+
 
         self.file = filePicker()
         self.crop_face()
@@ -104,9 +148,11 @@ class App():
         self.tk_img = ImageTk.PhotoImage(self.orig_img)
         w, h = self.orig_img.size
         self.mask = Image.new('L', self.orig_img.size, "black")
+        self.mask_draw = ImageDraw.Draw(self.mask)
+
         self.mask_path = None
-        self.canvas = tk.Canvas(master, width=w, height=h)
-        self.canvas.pack()
+        self.canvas_pic = tk.Canvas(master, width=w, height=h)
+        self.canvas_pic.pack()
         self.x = 0
         self.y = 0
         self.select_windows = []
@@ -115,23 +161,68 @@ class App():
         self.rect = None
         self.start_x = None
         self.start_y = None
-        self.canvas.bind("<ButtonPress-1>", self.on_button_press)
-        self.canvas.bind("<B1-Motion>", self.on_move_press)
-        self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
+        self.canvas_pic.bind("<ButtonPress-1>", self.on_button_press_on_brush_mode)
+        # self.canvas.bind("<B1-Motion>", self.on_move_press)
+        # self.canvas.bind("<ButtonRelease-1>", self.on_button_release)
         self._draw_image()
 
         self.face_coordinate = None
         self.points_pool = []
         # import_button = tk.Button(self.master, text="select photo", command=(lambda : SelectAndShowImage(root))).pack(fill=tk.X, side = tk.TOP, )
 
-        save_button = tk.Button(self.master, text="save masks", command=(self.save_mask)).pack(fill=tk.X,
-                                                                                               side=tk.TOP)  # this is intermediate , will delete later
-        restore_button = tk.Button(self.master, text="restore",
-                                   command=(lambda: inpaint(self.file, self.mask_path))).pack(fill=tk.X, side=tk.TOP)
 
 
-        self.master.mainloop()
 
+
+        # self.save_mask_button = tk.Button(self.master, text="Save Mask")
+        # self.save_mask_button.pack(side=tk.BOTTOM, fill=tk.X)
+        # self.restore_button = tk.Button(self.master, text="Restore")
+        # self.restore_button.pack(side=tk.BOTTOM, fill=tk.X)
+
+
+        scrollbar.config(command=self.canvas_pic.yview_moveto(100))
+        # self.master.mainloop()
+
+    def select_paintbrush(self):
+        if not self.brush_selected:
+            self.brush_button["image"] = self.brush_selected_image
+            self.brush_selected = True
+
+            self.toggle_on_paintbrush_slider()
+
+            self.rectangle_button["image"] = self.rectangle_not_selected_image
+            self.rectangle_selected = False
+
+    def select_rectangle_tool(self):
+        if not self.rectangle_selected:
+            self.rectangle_button["image"] = self.rectangle_selected_image
+            self.rectangle_selected = True
+
+            self.toggle_off_paintbrush_slider()
+
+            self.brush_button["image"] = self.brush_not_selected_image
+            self.brush_selected = False
+
+    def select_paintbrush_size(self):
+        pass
+
+    def toggle_off_paintbrush_slider(self):
+        self.brush_slider.pack_forget()
+        self.brush_size_label.pack_forget()
+
+    def toggle_on_paintbrush_slider(self):
+        self.brush_slider.pack()
+        self.brush_size_label.pack()
+
+    def change_slider_value(self, val):
+        self.brush_size_image = tk.PhotoImage(file="../assets/brushsize_" + str(val) + ".png")
+        self.brush_size_label["image"] = self.brush_size_image
+        print "change brush size to:",self.brush_slider.get()
+        self.brush_slider.set(self.brush_slider.get())
+        self.brush_size = self.brush_slider.get()
+
+    def close_window(self):
+        self.master.destroy()
     def crop_face(self):
         # Get user supplied values
         curr_path = os.getcwd()
@@ -172,19 +263,19 @@ class App():
         curr_path = os.getcwd()
         new_path = curr_path.replace("/python", "/data/images/mask/")
         oldfileName = (self.file)[(self.file).rfind("/") + 1:-4]
-        output = ImageOps.fit(self.mask, self.orig_img.size, centering=(0.5, 0.5))
-        draw = ImageDraw.Draw(self.mask)
-
-        draw.polygon(self.points_pool, fill="white")
-
-        output.paste(self.mask)
         self.mask_path = new_path + oldfileName + "_mask.png"
-        output.save(self.mask_path)
-        print "saved mask"
+        self.mask.save(self.mask_path)
+        print "mask saved "
 
     def _draw_image(self):
-        self.canvas.create_image(0, 0, anchor="nw", image=self.tk_img)
+        self.canvas_pic.create_image(0, 0, anchor="nw", image=self.tk_img)
 
+    def on_button_press_on_brush_mode(self, event):
+        print "drawing on", (event.x, event.y)
+        self.canvas_pic.create_rectangle(event.x-self.brush_size, event.y-self.brush_size,
+                                                event.x+self.brush_size, event.y+self.brush_size, fill="white")
+        self.mask_draw.rectangle([event.x-self.brush_size, event.y-self.brush_size,
+                                                event.x+self.brush_size, event.y+self.brush_size], fill="white")
     def on_button_press(self, event):
         # save mouse drag start position
         self.start_x = event.x
@@ -194,13 +285,13 @@ class App():
         print "appending start", (event.x, event.y)
         # create rectangle if not yet exist
         # if not self.rect:
-        self.rect = self.canvas.create_line(self.x, self.y, 1, 1, fill="white")
+        self.rect = self.canvas_pic.create_line(self.x, self.y, 1,1, fill="white")
 
     def on_move_press(self, event):
         curX, curY = (event.x, event.y)
 
         # expand rectangle as you drag the mouse
-        self.canvas.coords(self.rect, self.start_x, self.start_y, curX, curY)
+        self.canvas_pic.coords(self.rect, self.start_x, self.start_y, curX, curY)
 
     def on_button_release(self, event):
         # self.current_window.append((event.x,event.y))
@@ -215,3 +306,4 @@ class App():
 
 root = tk.Tk()
 App(root)
+root.mainloop()
